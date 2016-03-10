6a721978536934c11b3f2d09d087e747d0c64a70
Tarnum Geng <i@tianyi.world>
2016-03-10 15:45:24 +0800

    small modify

--- bitstream/bitstream.c
+++ bitstream/bitstream.c
@@ -1,5 +1,5 @@
+#include <stdlib.h>
 #include "bitstream.h"
-#include "stdlib.h"
 
 void open_bitstream(struct BitStream* bs, uint16_t size) {
 	bs->data = (unsigned char*) malloc(size * sizeof(unsigned char));
