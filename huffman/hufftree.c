6e8e27d3f6b5ef3ed698ccfde562fcc8787a1bf5
Tarnum Geng <i@tianyi.world>
2016-03-11 11:48:56 +0800

    small modify

--- huffman/hufftree.c
+++ huffman/hufftree.c
@@ -6,31 +6,31 @@ void build_tree(struct TreeNode* root, struct HuffCodeBook* book) {
 
 }
 
-static void _node_init(struct TreeNode* n) {
-	n->val = -1;
-	n->is_leaf = 0;
-	n->left = NULL;
-	n->right = NULL;
+static struct TreeNode* _new_node() {
+    struct TreeNode* n = (struct TreeNode*) malloc(sizeof(struct TreeNode));
+    n->val = -1;
+    n->is_leaf = 0;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
 }
 
 static void _tree_insert(struct TreeNode* root, uint16_t bitcnt, uint16_t code, uint16_t val) {
-	struct TreeNode* curr = root;
-	uint16_t i;
-	for (i = bitcnt - 1; i >= 0; i--) {
-		uint16_t choice = (code >> i) & 0x1;
-		if (choice) {
-			if (!curr->right) {
-				curr->right = (struct TreeNode*) malloc(sizeof(struct TreeNode));
-				_node_init(curr->right);
-			}
-			curr = curr->right;
-		} else {
-			if (!curr->left) {
-				curr->left = (struct TreeNode*) malloc(sizeof(struct TreeNode));
-				_node_init(curr->left);
-			}
-			curr = curr->left;
-		}
-	}
-	curr->is_leaf = 1;
+    struct TreeNode* curr = root;
+    uint16_t i;
+    for (i = bitcnt; i > 0; i--) {
+        uint16_t choice = (code >> i) & 0x1;
+        if (choice) {
+            if (!curr->right) {
+                curr->right = _new_node();
+            }
+            curr = curr->right;
+        } else {
+            if (!curr->left) {
+                curr->left = _new_node();
+            }
+            curr = curr->left;
+        }
+    }
+    curr->is_leaf = 1;
 }
