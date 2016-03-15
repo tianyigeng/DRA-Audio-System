8c9fd014bccbe892d175fe9ffc557705a06dbd67
Tarnum Geng <i@tianyi.world>
2016-03-15 16:42:14 +0800

    change indentation

--- util/errors.c
+++ util/errors.c
@@ -6,19 +6,19 @@
 #include "errors.h"
 
 const uint32_t ERROR_FAILURE_ALLOC_MEM = 0x500;
-const uint32_t ERROR_UNKNOWN 		   = 0x999;
+const uint32_t ERROR_UNKNOWN           = 0x999;
 
 void handle_error(uint32_t code) {
-	switch (code) {
-		case ERROR_FAILURE_ALLOC_MEM:
-		printf("memory allocation failure, exitting!");
-        exit(ERROR_FAILURE_ALLOC_MEM);
-		break;
+    switch (code) {
+        case ERROR_FAILURE_ALLOC_MEM:
+            printf("memory allocation failure, exitting!");
+            exit(ERROR_FAILURE_ALLOC_MEM);
+            break;
 
-		default:
-		printf("unknown error, exitting!");
-        exit(ERROR_UNKNOWN);
-	}
+        default:
+            printf("unknown error, exitting!");
+            exit(ERROR_UNKNOWN);
+    }
 }
 
 #endif
