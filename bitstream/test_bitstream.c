29489220dec7ca2e8c6142a0309c7f6cc9e92a3d
Tarnum Geng <i@tianyi.world>
2016-03-16 09:48:43 +0800

    update testing

--- bitstream/test_bitstream.c
+++ bitstream/test_bitstream.c
@@ -2,8 +2,12 @@
 #include "bitstream.h"
 
 int main() {
-	struct bit_stream* bs = bitstream_init();
-	bitstream_push(bs, 15, 15);
-	bitstream_print(bs);
-	bitstream_destroy(bs);
+    struct bit_stream* bs = bitstream_init();
+    bitstream_push(bs, 15, 15);
+    bitstream_push(bs, 1, 2);
+    bitstream_push(bs, 22, 20);
+    bitstream_push(bs, 122, 11);
+    bitstream_push(bs, 1023, 19);
+    bitstream_print(bs);
+    bitstream_destroy(bs);
 }
