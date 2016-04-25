f655f6ad165bdc419ab63d78cde21ab08e27977c
Tarnum Geng <i@tianyi.world>
2016-04-25 22:09:58 +0800

    share value between enc and dec

---
+++ dataframe/dataframe.h
@@ -0,0 +1,34 @@
+#ifndef __DATAFRAME_H_
+#define __DATAFRAME_H_
+
+/* some values shared by the encoder and decoder */
+
+#include <stdint.h>
+
+typedef int32_t   INT;
+typedef uint8_t   BOOL;
+typedef double    DOUBLE;
+
+const BOOL TRUE  = 1;
+const BOOL FALSE = 0;
+
+const INT WIN_LONG_LONG2LONG    = 0;   /* size: 2048 samples */
+const INT WIN_LONG_LONG2SHORT   = 1;   /* size: 2048 samples */
+const INT WIN_LONG_SHORT2LONG   = 2;   /* size: 2048 samples */
+const INT WIN_LONG_SHORT2SHORT  = 3;   /* size: 2048 samples */
+const INT WIN_LONG_LONG2BRIEF   = 4;   /* size: 2048 samples */
+const INT WIN_LONG_BRIEF2LONG   = 5;   /* size: 2048 samples */
+const INT WIN_LONG_BRIEF2BRIEF  = 6;   /* size: 2048 samples */
+const INT WIN_LONG_SHORT2BRIEF  = 7;   /* size: 2048 samples */
+const INT WIN_LONG_BRIEF2SHORT  = 8;   /* size: 2048 samples */
+const INT WIN_SHORT_SHORT2SHORT = 9;   /* size: 256 samples */
+const INT WIN_SHORT_SHORT2BRIEF = 10;  /* size: 256 samples */
+const INT WIN_SHORT_BRIEF2BRIEF = 11;  /* size: 256 samples */
+const INT WIN_SHORT_BRIEF2SHORT = 12;  /* size: 256 samples */
+
+#define MAX_INDEX   1024 /* max indices in a frame */
+#define MAX_CLUSTER 4    /* max clusters in a frame */
+#define MAX_BAND    32   /* max bands in a cluster */
+#define MAX_STEP    116  /* max step values for unit-step */
+
+#endif
--- dataframe/decode.c
+++ dataframe/decode.c
@@ -2,33 +2,12 @@
 #define __DECODE_C_
 
 #include <stdlib.h>
-#include <stdint.h>
 #include <assert.h>
 #include "../bitstream/bitstream.h"
 #include "../bitstream/bs_iter.h"
 #include "../huffman/huffbook.h"
 #include "../huffman/huffcoding.h"
-
-typedef int32_t   INT;
-typedef uint8_t   BOOL;
-typedef double    DOUBLE;
-
-const BOOL TRUE  = 1;
-const BOOL FALSE = 0;
-
-const INT WIN_LONG_LONG2LONG    = 0;   /* size: 2048 samples */
-const INT WIN_LONG_LONG2SHORT   = 1;   /* size: 2048 samples */
-const INT WIN_LONG_SHORT2LONG   = 2;   /* size: 2048 samples */
-const INT WIN_LONG_SHORT2SHORT  = 3;   /* size: 2048 samples */
-const INT WIN_LONG_LONG2BRIEF   = 4;   /* size: 2048 samples */
-const INT WIN_LONG_BRIEF2LONG   = 5;   /* size: 2048 samples */
-const INT WIN_LONG_BRIEF2BRIEF  = 6;   /* size: 2048 samples */
-const INT WIN_LONG_SHORT2BRIEF  = 7;   /* size: 2048 samples */
-const INT WIN_LONG_BRIEF2SHORT  = 8;   /* size: 2048 samples */
-const INT WIN_SHORT_SHORT2SHORT = 9;   /* size: 256 samples */
-const INT WIN_SHORT_SHORT2BRIEF = 10;  /* size: 256 samples */
-const INT WIN_SHORT_BRIEF2BRIEF = 11;  /* size: 256 samples */
-const INT WIN_SHORT_BRIEF2SHORT = 12;  /* size: 256 samples */
+#include "dataframe.h"
 
 /* Forward decls */
 static void     init(struct bit_stream* bs); /* set up essentials */
@@ -49,12 +28,6 @@ static void     UnpackJicScale();
 static void     UnpackBitPad();
 static void     AuxiliaryData();
 /* End of forward decls */
-
-
-#define MAX_INDEX   1024 /* max indices in a frame */
-#define MAX_CLUSTER 4    /* max clusters in a frame */
-#define MAX_BAND    32   /* max bands in a cluster */
-#define MAX_STEP    116  /* max step values for unit-step */
 
 /* an iterator of the input bitstream */
 struct bs_iter* iter;
